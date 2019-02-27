9b854fb6c45ac5b83fc4d7ed456e3a49b799bc52
amaurs33 <dufour.amaury@laposte.net>
2019-02-27 17:06:33 +0100

    création de la fentre pour les routes et de la fonctions d'affichage de retour de commande linux

--- gui.py
+++ gui.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 from Tkinter import *
+import subprocess
 
+
+### definition des fonctions ###
+### affichage commande ( route -n )
+def route() :
+
+	routePrint = subprocess.Popen(["route","-n"], stdout=subprocess.PIPE)
+	output = routePrint.communicate()[0]
+	print(output)
+	route_label.set(output)
 ## définition des actions nouvelle fenêtre
-
+## definition de la fenetre de configuration des routes ##
 def win_route() :
-	newwin = Toplevel(window)
-	display = Label(newwin, text="définiton nouvelle route")
-	display.pack()
+	
+
+	newwin = Toplevel()
+	frame_down = Frame (newwin,height=200,width=800,relief=RAISED,bd=8,bg="black")
+	frame_up = Frame (newwin,height=400,width=800,relief=SUNKEN,bd=8,bg="white")
+	frame_down.grid(row=1,column=0)
+	frame_up.grid(row=0,column=0)
+	newwin.title("Configurer des routes")
+	newwin.configure(bg='#ffffff')
+	newwin.geometry("800x600")
+
+	labelDown = Label(frame_down,textvariable=route_label)
+	labelDown.place(x=0,y=0)
+	labelDown.configure(foreground="white",bg='#000000')
+	route_label.set("")
+
+	buttonTest =Button(frame_down, text="Actualiser", foreground = "black", command=route)
+	buttonTest.place(x=690,y=0)
+
 
 # main 
 window = Tk()
-
+## définition des variables ##
+route_label = StringVar()
 ### mise en forme de la fenêtre principale ###
 
 window.title("titre de test")
