320b6ea33fcecc87ff6c81f01d2d57805933aa3e
amaurs33 <dufour.amaury@laposte.net>
2019-03-22 22:14:46 +0100

    Partie log fonctionnelle, mise en place d'une base de donnée SQlite

--- log.py
+++ log.py
@@ -2,10 +2,18 @@
 # -*- coding: latin-1 -*-
 from Tkinter import *
 import subprocess
+import sqlite3
 
 
 ############################################ definition des fonctions ####################################
+def ajoutDB ():
+	## gestion de la base de donnée log.db
 
+	connexion = sqlite3.connect("dataBase/log.db") #connexion à la base de donnée
+	donnees = (nouveau_chemin_log.get(),nom_chemin_log.get())
+	curseur.execute('''INSERT INTO logFiles (chemin, nom ) VALUES (?,?)''',donnees)
+	connexion.close()
+	
 def log() :
 
 
@@ -13,7 +21,6 @@ def log() :
 	output = logPrint.communicate()[0]
 	print(output)
 	log_label.set(output)
-
 
 
 ####################################### Fenêtre secondaire log #######################################
@@ -31,24 +38,37 @@ winlog.configure(bg='#ffffff')
 winlog.geometry("1000x500")
 winlog.resizable(width=False,height=False)
 
+### Définition des variables
+nouveau_chemin_log = StringVar()
+nouveau_chemin_log.set('')
+
+nom_chemin_log = StringVar()
+nom_chemin_log.set('')
+
+### gestion de la base de donnée
+connexion = sqlite3.connect("dataBase/log.db") #connexion à la base de donnée
+
+curseur.execute('''CREATE TABLE IF NOT EXISTS logFiles (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, chemin TEXT, nom TEXT)''') # création de la table logFiles
+connexion.close()
+
 ### ajout du label et du textbox
 
 labelLog = Label(frame_up, text="Ajouter un fichier de log :", foreground='white',bg='#1d83ff')
 labelLog.grid(row=1,column=0)
-entryLog = Entry(frame_up)
+entryLog = Entry(frame_up,textvariable=nouveau_chemin_log)
 entryLog.grid(row=1,column=1)
 labelVide = Label(frame_up,text="  ",bg='#ffffff')
 labelVide.grid(row=1,column=2)
 
 labelName = Label(frame_up, text="Donnez-lui un nom", foreground='white',bg='#1d83ff')
 labelName.grid(row=1,column=3)
-entryName = Entry(frame_up)
+entryName = Entry(frame_up,textvariable=nom_chemin_log)
 entryName.grid(row=1,column=4)
 
 labelVide = Label(frame_up,text="  ",bg='#ffffff') # Ces deux lignes permettent juste d'espacer les éléments
 labelVide.grid(row=1,column=5)
 
-buttonEnregistrer=Button(frame_up)
+buttonEnregistrer=Button(frame_up,command=ajoutDB)
 buttonEnregistrer.grid(row=1,column=6)
 buttonAddImg = PhotoImage(file="pictures/buttonAdd2.gif")
 buttonEnregistrer.config(image=buttonAddImg)
@@ -66,9 +86,6 @@ lb.insert(1,"message")
 lb.select_set(0)
 lb.grid(row=2,column=1)
 
-dc = {}
-dc['syslog'] = r'/var/log/syslog'
-dc['message'] = r'/var/log/message'
 
 selectItem = lb.get(lb.curselection())
 
--- scripts/log.py
+++ scripts/log.py
@@ -2,18 +2,59 @@
 # -*- coding: latin-1 -*-
 from Tkinter import *
 import subprocess
+import tkMessageBox
+import ttk
+import sqlite3
 
 
 ############################################ definition des fonctions ####################################
+def affichageLog():
 
-def log() :
 
-
-	logPrint = subprocess.Popen(["tail",cheminLog], stdout=subprocess.PIPE)
+	logPrint = subprocess.Popen(["tail",cheminLog.get()], stdout=subprocess.PIPE)
 	output = logPrint.communicate()[0]
 	print(output)
 	log_label.set(output)
 
+def recuperationCheminDB() :
+
+	connexion = sqlite3.connect("dataBase/log.db") #connexion à la base de donnée
+	curseur = connexion.cursor()
+	name = lb.get(lb.curselection())
+	curseur.execute("SELECT chemin FROM logFiles WHERE nom LIKE '%s'" % name)
+	record = curseur.fetchone()
+	cheminLog.set(record[0]) 
+	connexion.close()
+	affichageLog()
+
+def creationBD () :
+	connexion = sqlite3.connect("dataBase/log.db") #connexion à la base de donnée
+	curseur = connexion.cursor()
+	curseur.execute('''CREATE TABLE IF NOT EXISTS logFiles (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, chemin TEXT, nom TEXT)''') # création de la table logFiles
+	connexion.close()
+
+def ajoutDB ():
+	## gestion de la base de donnée log.db
+
+	connexion = sqlite3.connect("dataBase/log.db") #connexion à la base de donnée
+	curseur = connexion.cursor()
+	donnees = (nouveau_chemin_log.get(),nom_chemin_log.get())
+	curseur.execute('''INSERT INTO logFiles (chemin, nom ) VALUES (?,?)''',donnees)
+	connexion.commit()
+	tkMessageBox.showinfo("Réussite", "Opération effectuée")
+	connexion.close()
+
+
+
+def integrationListbox () :
+	connexion = sqlite3.connect("dataBase/log.db") #connexion à la base de donnée
+	curseur = connexion.cursor()
+	curseur.execute("SELECT nom FROM logFiles") # Récupération des valeurs "nom" dans la base de donnée pour l'insérer dans la listeBox
+	i=0
+	for row in curseur.fetchall():
+		lb.insert(i,row[0])
+		i=i+1
+	connexion.close()
 
 ####################################### Fenêtre secondaire log #######################################
 
@@ -30,24 +71,45 @@ winlog.configure(bg='#ffffff')
 winlog.geometry("1000x500")
 winlog.resizable(width=False,height=False)
 
+### Définition des variables
+nouveau_chemin_log = StringVar()
+nouveau_chemin_log.set('')
+
+nom_chemin_log = StringVar()
+nom_chemin_log.set('')
+
+cheminLog = StringVar()
+cheminLog.set('')
+
+### création de la listbox pour choisir le log à afficher ###
+
+lb = Listbox(frame_up, height =2)
+lb.grid(row=2,column=1)
+
+### gestion de la base de donnée
+creationBD ()
+integrationListbox ()
+
+#recuperationCheminDB()
+
 ### ajout du label et du textbox
 
 labelLog = Label(frame_up, text="Ajouter un fichier de log :", foreground='white',bg='#1d83ff')
 labelLog.grid(row=1,column=0)
-entryLog = Entry(frame_up)
+entryLog = Entry(frame_up,textvariable=nouveau_chemin_log)
 entryLog.grid(row=1,column=1)
 labelVide = Label(frame_up,text="  ",bg='#ffffff')
 labelVide.grid(row=1,column=2)
 
 labelName = Label(frame_up, text="Donnez-lui un nom", foreground='white',bg='#1d83ff')
 labelName.grid(row=1,column=3)
-entryName = Entry(frame_up)
+entryName = Entry(frame_up,textvariable=nom_chemin_log)
 entryName.grid(row=1,column=4)
 
 labelVide = Label(frame_up,text="  ",bg='#ffffff') # Ces deux lignes permettent juste d'espacer les éléments
 labelVide.grid(row=1,column=5)
 
-buttonEnregistrer=Button(frame_up)
+buttonEnregistrer=Button(frame_up,command=ajoutDB)
 buttonEnregistrer.grid(row=1,column=6)
 buttonAddImg = PhotoImage(file="pictures/buttonAdd2.gif")
 buttonEnregistrer.config(image=buttonAddImg)
@@ -56,25 +118,9 @@ buttonEnregistrer.image = buttonAddImg
 labelName = Label(frame_up, text="Sélectionnez un Log:", foreground='white',bg='#6a8bff')
 labelName.grid(row=2,column=0)
 
-### création de la listbox pour choisir le log à afficher ###
 
 
-lb = Listbox(frame_up, height =2)
-lb.insert(0, "syslog")
-lb.insert(1,"message")
-lb.select_set(0)
-lb.grid(row=2,column=1)
-
-dc = {}
-dc['syslog'] = r'/var/log/syslog'
-dc['message'] = r'/var/log/message'
-
-selectItem = lb.get(lb.curselection())
-
-cheminLog=dc[selectItem]
-print cheminLog
-
-buttonAfficher=Button(frame_up, command=log)
+buttonAfficher=Button(frame_up, command=recuperationCheminDB)
 buttonAfficher.grid(row=2,column=2)
 afficherImg = PhotoImage(file="pictures/afficher.gif")
 buttonAfficher.config(image=afficherImg)
